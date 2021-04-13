dae8f3f32a700fc82d5d01a05fc98bf8b7df2c34
leventefrks <x-farkasle@wizzair.com>
2021-04-13 20:54:33 +0200

    static footer, secondary menu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,89 +1,114 @@
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaYoutubeSquare,
+} from 'react-icons/fa';
+
 const Footer = ({ children }) => {
   return (
-    <footer className="max-w-5xl mx-auto mb-12 text-gray-400 grid grid-cols-4 grid-flow-row text-sm">
-      <ul className="flex flex-col capitalize space-y-4">
+    <footer className="max-w-5xl mx-auto mb-12 text-gray-400">
+      <ul className="flex space-x-5 mb-3">
         <li>
-          <a className="hover:underline" href="">
-            audio and aubtitles
+          <a href="">
+            <FaFacebookSquare className="w-8 h-8 fill-current" />
           </a>
         </li>
         <li>
-          <a className="hover:underline" href="">
-            media center
+          <a href="">
+            <FaInstagramSquare className="w-8 h-8 fill-current" />
           </a>
         </li>
         <li>
-          <a className="hover:underline" href="">
-            privacy
-          </a>
-        </li>
-        <li>
-          <a className="hover:underline" href="">
-            contact us
+          <a href="">
+            <FaYoutubeSquare className="w-8 h-8 fill-current" />
           </a>
         </li>
       </ul>
-      <ul className="flex flex-col capitalize space-y-4">
-        <li>
-          <a className="hover:underline" href="">
-            audio description
-          </a>
-        </li>
-        <li>
-          <a className="hover:underline" href="">
-            investor relations
-          </a>
-        </li>
-        <li>
-          <a className="hover:underline" href="">
-            legal notices
-          </a>
-        </li>
-        <li>
-          <a className="hover:underline" href="">
-            contact us
-          </a>
-        </li>
-      </ul>
-      <ul className="flex flex-col capitalize space-y-4">
-        <li>
-          <a className="hover:underline" href="">
-            audio and aubtitles
-          </a>
-        </li>
-        <li>
-          <a className="hover:underline" href="">
-            help center
-          </a>
-        </li>
-        <li>
-          <a className="hover:underline" href="">
-            jobs
-          </a>
-        </li>
-        <li>
-          <a className="hover:underline" href="">
-            cookie preferencies
-          </a>
-        </li>
-      </ul>
-      <ul className="flex flex-col capitalize space-y-4">
-        <li>
-          <a className="hover:underline" href="">
-            gift cards
-          </a>
-        </li>
-        <li>
-          <a className="hover:underline" href="">
-            terms of use
-          </a>
-        </li>
-        <li>
-          <a className="hover:underline" href="">
-            corporate information
-          </a>
-        </li>
-      </ul>
+      <div className="grid grid-cols-4 grid-flow-row text-sm">
+        <ul className="flex flex-col capitalize space-y-4">
+          <li>
+            <a className="hover:underline" href="">
+              audio and aubtitles
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              media center
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              privacy
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              contact us
+            </a>
+          </li>
+        </ul>
+        <ul className="flex flex-col capitalize space-y-4">
+          <li>
+            <a className="hover:underline" href="">
+              audio description
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              investor relations
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              legal notices
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              contact us
+            </a>
+          </li>
+        </ul>
+        <ul className="flex flex-col capitalize space-y-4">
+          <li>
+            <a className="hover:underline" href="">
+              audio and aubtitles
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              help center
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              jobs
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              cookie preferencies
+            </a>
+          </li>
+        </ul>
+        <ul className="flex flex-col capitalize space-y-4">
+          <li>
+            <a className="hover:underline" href="">
+              gift cards
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              terms of use
+            </a>
+          </li>
+          <li>
+            <a className="hover:underline" href="">
+              corporate information
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
--- src/components/SecondaryMenu.js
+++ src/components/SecondaryMenu.js
@@ -1,11 +1,17 @@
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaGift, FaBell } from 'react-icons/fa';
 
 export default function SecondaryMenu() {
   return (
-    <ul className="flex items-center space-x-5">
-      <li className="flex items-center">
+    <ul className="flex items-center">
+      <li className="flex items-center space-x-9">
         <a href="#" className="text-white">
-          <FaSearch className="w-4 h-4 fill-current" />
+          <FaSearch className="w-6 h-6 fill-current" />
+        </a>
+        <a href="#" className="text-white">
+          <FaGift className="w-6 h-6 fill-current" />
+        </a>
+        <a href="#" className="text-white">
+          <FaBell className="w-6 h-6 fill-current" />
         </a>
       </li>
     </ul>
